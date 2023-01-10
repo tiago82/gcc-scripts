aee138553160c4dc8390f0cfe97d44ffd4ad8ce3
Tiago <tiago8204@gmail.com>
2023-01-10 16:07:09 -0300

    Update j700-18-1.sh

--- j700-18-1.sh
+++ j700-18-1.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+bash <(curl -s https://raw.githubusercontent.com/tiago82/gcp-scripts/main/2.sh)
+
 cd $HOME/los
+
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung/ -b lineage-18.1  vendor/samsung/
 
 source build/envsetup.sh
 breakfast j7elte
+croot
+brunch j7elte
+
+sudo poweroff
