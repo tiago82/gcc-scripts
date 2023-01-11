f6a16027b434cde3387966ad0fe0c37abd14a6a0
Tiago <tiago8204@gmail.com>
2023-01-11 19:24:18 -0300

    Update j700-20-0.sh

--- j700-20-0.sh
+++ j700-20-0.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cd $HOME/los
+
+
+git clone https://github.com/LineageOS/android_device_samsung_j7elte device/samsung/j7elte
+git clone https://github.com/LineageOS/android_device_samsung_universal7580-common device/samsung/universal7580-common
 git clone https://github.com/LineageOS/android_hardware_samsung hardware/samsung
 git clone https://github.com/LineageOS/android_hardware_samsung_nfc hardware/samsung/nfc
 git clone https://github.com/LineageOS/android_hardware_samsung_slsi_openmax hardware/samsung_slsi/openmax
@@ -9,6 +14,6 @@ git clone https://github.com/LineageOS/android_hardware_samsung_slsi_exynos7580 
 git clone https://github.com/LineageOS/android_kernel_samsung_universal7580 kernel/samsung/universal7580
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung/ -b lineage-18.1  vendor/samsung/
 
-cd $HOME/los
+
 source build/envsetup.sh
 breakfast j7elte
